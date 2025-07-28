0b876cdb3f37a69a7a6353612ba73c23e9dbe18d
shimamura <shimakou779@gmail.com>
2025-07-28 14:19:16 +0900

    [update]移動できるようにした

--- Assets/Script/test Player.cs
+++ Assets/Script/test Player.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class testPlayer : MonoBehaviour
 {
-    private InputMap _inputMap; 
+    private Rigidbody _rb;
+    private InputMap _inputMap;
+
+    [SerializeField]
+    public float moveSpeed = 5f;
+    private Vector2 _InputDirectoin;
+
+    private void Awake()
+    {
+        _inputMap = new InputMap();
+
+        // Move入力の値を受け取る設定
+        _inputMap.Player.Move.performed += ctx => _InputDirectoin = ctx.ReadValue<Vector2>();
+        _inputMap.Player.Move.canceled += ctx => _InputDirectoin = Vector2.zero;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        _inputMap = new InputMap();
+        _rb = GetComponent<Rigidbody>();
         _inputMap.Enable();
     }
 
@@ -20,5 +35,20 @@ public class testPlayer : MonoBehaviour
         {
             Debug.Log("ファイヤー");
         }
+        if(_inputMap.Player.Move.triggered)
+        {
+           
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 currentVelocity = _rb.velocity;
+        Vector3 move = new Vector3(_InputDirectoin.x * moveSpeed, currentVelocity.y, _InputDirectoin.y * moveSpeed);
+        _rb.velocity = move;
+    }
+
+    private void OnDisable()
+    {
+        _inputMap.Disable();
     }
 }
