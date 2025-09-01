4536cdac9c7de4e279f6759a3d78aaf3dc752298
shimamura <shimakou779@gmail.com>
2025-09-01 20:19:33 +0900

    [update]プレイヤーを改善途中

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,31 +7,35 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     public int _MoveSpeed;
-    private Vector3 _inputDirection;
+    private Vector2 _inputDirection;
 
-    public Rigidbody _rg = null;
+    public Rigidbody _rd;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
 
-        _rg = GetComponent<Rigidbody>();
+        _rd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
-    public void Move()
+
+    private void FixedUpdate()
     {
-        _rg.velocity = new Vector3(_inputDirection.x * _MoveSpeed, _rg.velocity.y, _inputDirection.z);
+        Vector3 move = new Vector3(_inputDirection.x,0f,_inputDirection.y);
+        _rd.MovePosition(_rd.position + move * _MoveSpeed * Time.fixedDeltaTime);
     }
 
 
     public void OnMove(InputAction.CallbackContext context)        //InputAction.CallbackContextはInput Systemで発生したイベントを取得するためのもの
     {
-        _inputDirection = context.ReadValue<Vector3>();             //contextをVector2型に変換した値を_inputDirectionに代入している　
+
+        _inputDirection = context.ReadValue<Vector2>();             //contextをVector2型に変換した値を_inputDirectionに代入している　
                                                                     //つまり、右入力なら（1,0）左入力なら（-1,0）を代入している
+        Debug.Log("aaa");
     }
 }
