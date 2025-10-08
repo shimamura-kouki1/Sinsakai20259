0cf48731965744df8a50f4b2f145bb49a35fdd9c
shimamura <shimakou779@gmail.com>
2025-10-08 15:26:53 +0900

    [add]エネミーのアセット追加＋コライダーの調整をしたほうがいい

--- Assets/Script/Enemy2.cs
+++ Assets/Script/Enemy2.cs
@@ -10,6 +10,7 @@ public class Enemy2 : MonoBehaviour
     [SerializeField] private float _rayDistance = 1.5f; //オブジェクトの検知範囲
 
     private Rigidbody _rb;
+    private Transform _tr;
     private float _speed;//エネミーの総合的なスピード
     private float _timer;//寿命をカウントするためのもの
     private bool _isRotation = false;//回転中かどうか
@@ -19,6 +20,7 @@ public class Enemy2 : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _tr = GetComponent<Transform>();
 
         _speed = _playerBaseSpeed * _enemySpeed;//速度計算　プレイヤー速度*倍率
 
@@ -39,10 +41,10 @@ public class Enemy2 : MonoBehaviour
         if (_isRotation)//回転中
         {
             // 旋回処理
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRot, _rotationSpeed * Time.fixedDeltaTime);
+            _tr.rotation = Quaternion.RotateTowards(_tr.rotation, _targetRot, _rotationSpeed * Time.fixedDeltaTime);
 
             // 目標角度にほぼ到達したら旋回終了
-            if (Quaternion.Angle(transform.rotation, _targetRot) < 1f)
+            if (Quaternion.Angle(_tr.rotation, _targetRot) < 1f)
             {
                 _isRotation = false;
             }
@@ -50,24 +52,24 @@ public class Enemy2 : MonoBehaviour
             return; // 旋回中は前進しない
         }
 
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, _rayDistance))//前方にレイの生成
+        if (Physics.Raycast(_tr.position, _tr.forward, out RaycastHit hit, _rayDistance))//前方にレイの生成
         {
             if (hit.collider.CompareTag("Furniture"))//ヒットしたコライダーのタグが一致したら
             {
                 _turnDirection = Random.Range(0, 2) == 0 ? 1 : -1; //左右ランダムに回避できるように 1 = 右, -1 = 左
 
                 // 直角方向を計算している
-                _targetRot = Quaternion.LookRotation(Vector3.Cross(transform.forward, Vector3.up).normalized * _turnDirection, Vector3.up);
+                _targetRot = Quaternion.LookRotation(Vector3.Cross(_tr.forward, Vector3.up).normalized * _turnDirection, Vector3.up);
             }
             _isRotation = true;//回避動作に入る
             return;//旋回中は移動しないように
         }
-        _rb.MovePosition(_rb.position + transform.forward * _speed * Time.fixedDeltaTime);//前進し続ける
+        _rb.MovePosition(_rb.position + _tr.forward * _speed * Time.fixedDeltaTime);//前進し続ける
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Vector3 reflectDir = Vector3.Reflect(transform.forward, collision.contacts[0].normal);//反射のベクトル計算
-        transform.rotation = Quaternion.LookRotation(reflectDir, Vector3.up);//反射方向に回転
+        Vector3 reflectDir = Vector3.Reflect(_tr.forward, collision.contacts[0].normal);//反射のベクトル計算
+        _tr.rotation = Quaternion.LookRotation(reflectDir, Vector3.up);//反射方向に回転
     }
 }
